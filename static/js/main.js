eb7ad361d372e93875266f4d6f7c79e5c93ce0e3
Chhrone <rayinailham9@gmail.com>
2023-11-15 16:37:48 +0700

    commit

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,7 @@
 function addToOrder(name, price) {
     // Implement the logic to add to the order here
+    // You might want to store the quantity and other details here
+    // For simplicity, let's assume quantity is always 1 in this example
     fetch('/add-to-order', {
         method: 'POST',
         headers: {
@@ -8,6 +10,7 @@ function addToOrder(name, price) {
         body: JSON.stringify({
             name: name,
             price: price,
+            quantity: 1, // Adjust as needed
         }),
     })
     .then(response => {
@@ -33,9 +36,39 @@ function getOrderDetails() {
         order.items.forEach(item => {
             const foodCardElement = document.createElement('div');
             foodCardElement.classList.add('food-card');
-            foodCardElement.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+            foodCardElement.textContent = `${item.name} - Quantity: ${item.quantity} - $${(item.price * item.quantity).toFixed(2)}`;
             orderDetailsElement.appendChild(foodCardElement);
         });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+function placeOrder() {
+    // Implement the logic to place the order
+    const userName = document.getElementById('user-name').value;
+    const tableNumber = document.getElementById('table-number').value;
+
+    // You may want to include additional validation here
+    
+    // Send the user name and table number to the server
+    fetch('/place-order', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            userName: userName,
+            tableNumber: tableNumber,
+        }),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to place order');
+        }
+        // Optionally, you can reset the order details after placing the order
+        getOrderDetails();
     })
     .catch(error => {
         console.error('Error:', error);
